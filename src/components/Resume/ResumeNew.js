24d3dcfa274a577265f5eb8e8b9850a67fcc906c
sidhu-26 <siddaarth26@gmail.com>
2026-04-03 14:24:43 +0530

    refactor: replace react-pdf with iframe for external resume preview and remove unused state logic

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -1,19 +1,13 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/Siddhaarth_PT.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
-import { Document, Page, pdfjs } from "react-pdf";
-import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+const resumeLink = "https://drive.google.com/uc?id=1UlBPmsOS3vO-AaCsh0Yx8Mrk50XOuP-9&export=download";
+const previewLink = "https://drive.google.com/file/d/1UlBPmsOS3vO-AaCsh0Yx8Mrk50XOuP-9/preview";
 
 function ResumeNew() {
-  const [width, setWidth] = useState(1200);
-
-  useEffect(() => {
-    setWidth(window.innerWidth);
-  }, []);
 
   return (
     <div>
@@ -22,7 +16,7 @@ function ResumeNew() {
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={resumeLink}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
@@ -31,16 +25,26 @@ function ResumeNew() {
           </Button>
         </Row>
 
-        <Row className="resume">
-          <Document file={pdf} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
-          </Document>
+        <Row className="resume" style={{ justifyContent: "center", position: "relative" }}>
+          <iframe
+            src={previewLink}
+            width="100%"
+            height="1100px"
+            title="Resume Preview"
+            style={{ 
+              border: "none", 
+              borderRadius: "10px", 
+              maxWidth: "800px", 
+              background: "white", 
+              boxShadow: "0 10px 30px rgba(0,0,0,0.5)" 
+            }}
+          ></iframe>
         </Row>
 
         <Row style={{ justifyContent: "center", position: "relative" }}>
           <Button
             variant="primary"
-            href={pdf}
+            href={resumeLink}
             target="_blank"
             style={{ maxWidth: "250px" }}
           >
